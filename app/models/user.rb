44eb5d9938a4d4e4607118b770b7dc8b0164a57d
ed1ff <edifei13@yandex.ua>
2018-07-31 16:47:19 +0300

    add session create action

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
 
   validates :password, :password_confirmation, presence: true, confirmation: true, length: { minimum: 8 }, on: :create
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+
+  def authenticate!
+    regenerate_auth_token
+    auth_token
+  end
 end
