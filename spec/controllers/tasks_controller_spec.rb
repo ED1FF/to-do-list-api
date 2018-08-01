3e7a4b7b619c8364e43a61b2dba73712f2f0899c
ed1ff <edifei13@yandex.ua>
2018-08-01 13:43:32 +0300

    fix specs for tasks_controller add factories for users

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,41 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
-  let!(:task) { create(:task) }
+  let!(:user) { create(:user_with_tasks) }
+  let!(:task) { user.tasks.first }
+  let(:valid_session) { {Authorization: user.auth_token} }
   let(:valid_params) { attributes_for(:task) }
   let(:invalid_params) { { name: '', description: '' } }
 
   describe "#index" do
-    subject { get :index }
+    context "with auth" do
+      subject { get :index }
+      before { request.headers.merge! valid_session }
+      it { is_expected.to have_http_status(:ok) }
+    end
 
-    it { is_expected.to have_http_status(:ok) }
+    context "without auth" do
+      subject { get :index }
+      it { is_expected.to have_http_status(:unauthorized) }
+    end
   end
 
   describe "#show" do
-    context 'with valid id' do
+    context 'with valid id; with auth;' do
       subject { get :show, params: { id: task.id } }
+      before { request.headers.merge! valid_session }
 
       it { is_expected.to serialize_object(task).with(TaskSerializer) }
       it { is_expected.to have_http_status(:ok) }
     end
 
-    context 'with invalid id' do
+    context 'with invalid id; with auth;' do
       subject { get :show, params: { id: rand(1000) } }
+      before { request.headers.merge! valid_session }
 
       it { is_expected.to have_http_status(:not_found) }
+    end
+
+    context 'with valid id; without auth' do
+      subject { get :show, params: { id: rand(1000) } }
+      it { is_expected.to have_http_status(:unauthorized) }
     end
   end
 
   describe '#destroy' do
     subject { delete :destroy, params: { id: task.id } }
+    before { request.headers.merge! valid_session }
 
     it { is_expected.to have_http_status(:accepted) }
-    it { expect { delete :destroy, params: { id: task.id } }.to change(Task, :count).by(-1) }
+    it { expect { delete :destroy, params: { id: task.id } }.to change(user.tasks, :count).by(-1) }
   end
 
   describe '#update' do
-    context 'with incorect params' do
+    context 'with corect params' do
       subject { patch :update, params: { id: task.id , task: valid_params } }
+      before { request.headers.merge! valid_session }
 
       it { is_expected.to have_http_status(:accepted) }
       context 'update columns' do
@@ -50,6 +68,7 @@ RSpec.describe TasksController, type: :controller do
 
     context 'with incorect params' do
       subject { patch :update, params: { id: task.id , task: invalid_params } }
+      before { request.headers.merge! valid_session }
 
       it { is_expected.to have_http_status(:unprocessable_entity) }
     end
@@ -58,12 +77,14 @@ RSpec.describe TasksController, type: :controller do
   describe '#create' do
     context 'with incorect params' do
       subject { post :create, params: { task: valid_params } }
+      before { request.headers.merge! valid_session }
 
       it { is_expected.to have_http_status(:created) }
     end
 
     context 'with incorect params' do
       subject { post :create, params: { task: invalid_params } }
+      before { request.headers.merge! valid_session }
 
       it { is_expected.to have_http_status(:unprocessable_entity) }
     end
