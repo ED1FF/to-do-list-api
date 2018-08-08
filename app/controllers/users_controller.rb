ebd54892e177c965f240c82e817b83e03b831948
ed1ff <edifei13@yandex.ua>
2018-08-08 16:25:46 +0300

    add creating multiply address for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,13 @@ class UsersController < ApplicationController
   expose :user
 
   def create
-    return render json: { token: user.auth_token }, status: :created if user.update(user_params)
+    return render json: { token: user.auth_token }, status: :created if user.save
     render json: { status: :error, errors: user.errors.messages }, status: :unprocessable_entity
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:email, :token, :password, :password_confirmation, address_attributes: {})
+    params.require(:user).permit(:email, :token, :password, :password_confirmation, addresses_attributes: %i[city address zip])
   end
 end
