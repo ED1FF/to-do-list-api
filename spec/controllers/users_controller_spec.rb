840232ea2a5b378ed627c041dd97efd777f9a1fa
ed1ff <edifei13@yandex.ua>
2018-08-09 10:41:23 +0300

    add specs for user controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  let(:user) { create(:user) }
+  let!(:user) { create(:user) }
   let!(:correct_params) { { user: { email: Faker::Internet.email, password: '123321123321', password_confirmation: '123321123321' } } }
   let(:addresses_attributes) { attributes_for(:address) }
   let!(:incorrect_params) { { user: { email: '', password: '', password_confirmation: '' } } }
   let!(:user_params_with_address) { { user: { email: Faker::Internet.email, password: '123321123321', password_confirmation: '123321123321',addresses_attributes: [addresses_attributes] } } }
+  let!(:email) { Faker::Internet.email }
+  let(:user_update_params) { { id: user.id, user: { email: email } } }
+
   describe '#create' do
     context 'with correct credentials' do
       subject { post :create, params: correct_params }
@@ -20,5 +23,15 @@ RSpec.describe UsersController, type: :controller do
       it { is_expected.to have_http_status(:created) }
       it { expect { post :create, params: user_params_with_address }.to change(Address, :count).by(1) }
     end
+  end
+
+  describe '#show' do
+    subject { get :show, params: { id: user.id } }
+    it { is_expected.to have_http_status(:ok) }
+  end
+
+  describe '#update' do
+    subject { patch :update, params: user_update_params }
+    it { is_expected.to have_http_status(:accepted) }
   end
 end
